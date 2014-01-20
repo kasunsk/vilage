9aabfad4d26fc1d19cd7809cf6b447cd939cc5c0
kasunsk <kasunsk@gmail.com>
2014-01-20 22:28:38 +0530

    Selection was modified

--- src/main/java/windows/Selection.java
+++ src/main/java/windows/Selection.java
@@ -7,7 +7,12 @@ import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
 
+import main.java.windows.AddPersion;
+
 public class Selection {
+	
+	static Selection window = new Selection();
+	static AddPersion addpersion = new AddPersion();
 
 	protected Shell shell;
 
@@ -15,9 +20,9 @@ public class Selection {
 	 * Launch the application.
 	 * @param args
 	 */
-	public static void main(String[] args) {
+	public static void main(String [] args) {
 		try {
-			Selection window = new Selection();
+			//Selection window = new Selection();
 			window.open();
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -47,16 +52,8 @@ public class Selection {
 		shell.setSize(450, 300);
 		shell.setText("SWT Application");
 		
-		Button btnGo = new Button(shell, SWT.NONE);
-		btnGo.addSelectionListener(new SelectionAdapter() {
-			@Override
-			public void widgetSelected(SelectionEvent e) {
-			}
-		});
-		btnGo.setBounds(68, 131, 91, 29);
-		btnGo.setText("Go");
-		
-		Button btnAddAPersion = new Button(shell, SWT.RADIO);
+
+		final Button btnAddAPersion = new Button(shell, SWT.RADIO);
 		btnAddAPersion.setBounds(32, 35, 114, 24);
 		btnAddAPersion.setText("Add a Persion");
 		
@@ -71,6 +68,23 @@ public class Selection {
 		Button btnSerchAHome = new Button(shell, SWT.RADIO);
 		btnSerchAHome.setBounds(189, 65, 114, 24);
 		btnSerchAHome.setText("Serch a Home");
+		
+		Button btnGo = new Button(shell, SWT.NONE);
+		btnGo.addSelectionListener(new SelectionAdapter() {
+			
+		//Selection window = new Selection();
+			
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				if(btnAddAPersion.getSelection()){
+					window.shell.close();
+					addpersion.open();
+					
+				}
+			}
+		});
+		btnGo.setBounds(68, 131, 91, 29);
+		btnGo.setText("Go");
 
 	}
 }
